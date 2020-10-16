2d03ccd8f4a1d668e96734c1715298f61f3a72a8
Tyson Bailey <onaclov2000+github@gmail.com>
2020-10-15 18:07:39 -0600

    Update train.py

--- train.py
+++ train.py
@@ -4,6 +4,9 @@ import threading
 import socket
 import struct
 import time
+import datetime
+import json
+import os
 print ("input finished")
 import sys
 
@@ -27,6 +30,10 @@ class joystickEventSender(threading.Thread):
             self.JoystickData.append([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
        	threading.Thread.__init__(self)
     def run(self):
+        if os.path.exists('trainer_tracking.json'):
+            tracking = json.load(open('trainer_tracking.json', 'r'))
+        else:
+            tracking = {}
         #while self.running:
         x = 0
         presses = ["Horizontal","Vertical", "Start/Select", "B", "X", "A", "Y", "L", "R"]
@@ -86,12 +93,19 @@ class joystickEventSender(threading.Thread):
                     goal_states[-1][index_move[j]] = 1
 
         start_time = time.clock()
+        time_index = str(datetime.datetime.now())
+        # Add tracking of what sequence of buttons were pressed
+        if time_index not in tracking:
+            tracking[time_index] = {"actual_sequence" : [], "step_time" : [], "count" : 0, "sequence_time" : 0, "total_time" : 0}
         idx = 0
         print ("First Sequence", sequence[idx])
         sequence_incomplete = True
+        time.sleep(.03)
+        pygame.event.pump()
+        step_timer = time.clock()
+        sequence_timer = time.clock()
+
         while sequence_incomplete:
-            time.sleep(.03)
-            pygame.event.pump()
 
             self.JoystickData = normalize_data([pygame.joystick.Joystick(x).get_axis(0), # Left / Right (-1,1)
                     pygame.joystick.Joystick(x).get_axis(1), # Up, Down (-1,1)
@@ -103,13 +117,27 @@ class joystickEventSender(threading.Thread):
                     pygame.joystick.Joystick(x).get_button(4), # L bumper
                     pygame.joystick.Joystick(x).get_button(5), # R bumper
                     ])
-
+            
+            tracking[time_index]["actual_sequence"].append(goal_states[idx])
+            
+            # Check to see if we matched the goal state
             if self.JoystickData == goal_states[idx]:
                 #print ("Sequence", idx, "Complete")
+                tracking[time_index]["step_time"].append(time.clock() - step_timer)
                 idx += 1
+                step_timer = time.clock()
                 if len(goal_states) <= idx:
-                    print ("It took you ", time.clock() - start_time, "seconds from the start to end of the move doing it correctly.")
+#                    print ("It took you ", time.clock() - start_time, "seconds from the start to end of the move doing it correctly.")
+                    tracking[time_index]["sequence_time"] = time.clock() - sequence_timer
+                    tracking[time_index]["total_time"] = time.clock() - start_time
                     sequence_incomplete = False
+                    print (tracking)
+                    f = open("trainer_tracking.json", "w")
+                    f.write(json.dumps(tracking, sort_keys = True, indent = 2))
+                    f.close()
+                    
+                    for i in tracking[time_index]:
+                        print (i, tracking[time_index][i])
                     break
                 print ("Next Sequence", sequence[idx])
             else:
@@ -120,23 +148,14 @@ class joystickEventSender(threading.Thread):
                         # I think this means we pressed a different key?
                         print ("You pressed an incorrect key sequence Restarting!")
                         idx = 0
+                        tracking[time_index]["count"] += 1
                         print ("First Sequence", sequence[idx])
-                        start_time = time.clock()
-            #else:
-                #print (self.JoystickData, goal_states[idx])
-            if False:
-                #print (self.JoystickData)
-                for i in range(len(self.JoystickData)):
-                    if abs(self.JoystickData[i] - self.PriorData[i]) > .001:
-                        if abs(self.PriorData[i]) > 0.01:
-                            #timer[i] = time.start
-                            duration = time.clock() - timer[i]
-                            print (presses[i], duration)
-                            if abs(goal_time[i] - duration) < .001:
-                                print ("Achieved!")
-                        else:
-                            timer[i] = time.clock()
-                        self.PriorData[i] = self.JoystickData[i]
+                        step_timer = time.clock()
+                        sequence_timer = time.clock()
+            time.sleep(.03)
+            pygame.event.pump()
                 
 thread = joystickEventSender()
 thread.start()
+
+
